5aca5f85db1304a98a427ca7e0b54d6f78f49437
DavidRMorphew <morphewd@umich.edu>
2021-05-22 23:07:24 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,22 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+class DigitalClicker extends Component {
+    
+    state = {
+        timesClicked: 0
+    }
+
+    handleClick = (e) => {
+        console.log(this.state.timesClicked)
+        this.setState(prevState => ({timesClicked: prevState.timesClicked + 1}))
+    }
+    
+    render(){
+        return(   
+            <div>
+                <button className="button" onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
+export default DigitalClicker
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,10 @@
-// Code YouTubeDebugger Component Here
+
+import React, { Component } from 'react'
+class YouTubeDebugger extends Component {
+    render(){
+        return(   
+            <div>debug</div>
+        )
+    }
+}
+export default YouTubeDebugger
